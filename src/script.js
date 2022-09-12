cf2877e8e3cfff328a32b033c2ea5070dd7eebfa
Melandr <melnychenko.a@gmail.com>
2022-09-12 21:28:39 +0300

    add key & check errors

--- src/script.js
+++ src/script.js
@@ -1,5 +1,81 @@
 const calc = document.querySelector(".calc");
+const result = document.querySelector("#result");
 
-calc.addEventListener("click", function (event) {
-  if (event.target.classList.contains("calc__btn")) return;
-});
+calc.addEventListener("click", calculation);
+document.addEventListener("keydown", calculation);
+
+function calculation(event) {
+  if (!(event.target.classList.contains("calc__btn") || event.key.match(/[0-9%\/*\-+\(\)=]|Backspace|Enter|Delete/)))
+    return;
+
+  let valueInput = event.target.value || event.key;
+  switch (valueInput) {
+    case "C":
+    case "Delete":
+      clearDisplay();
+      break;
+    case "=":
+    case "Enter":
+      if (result.innerText.search(/[^0-9*/+-.]/im) != -1) return;
+      if (result.innerText.startsWith("/")) {
+        errorAlert();
+      } else if (
+        result.innerText.includes("--") ||
+        result.innerText.includes("-+") ||
+        result.innerText.includes("-.") ||
+        result.innerText.includes("-*") ||
+        result.innerText.includes("-/") ||
+        result.innerText.includes("+-") ||
+        result.innerText.includes("++") ||
+        result.innerText.includes("+.") ||
+        result.innerText.includes("+*") ||
+        result.innerText.includes("+/") ||
+        result.innerText.includes("*-") ||
+        result.innerText.includes("*+") ||
+        result.innerText.includes("*.") ||
+        result.innerText.includes("**") ||
+        result.innerText.includes("*/") ||
+        result.innerText.includes("/-") ||
+        result.innerText.includes("/+") ||
+        result.innerText.includes("/.") ||
+        result.innerText.includes("/*") ||
+        result.innerText.includes("//") ||
+        result.innerText.includes(".-") ||
+        result.innerText.includes(".+") ||
+        result.innerText.includes("..") ||
+        result.innerText.includes(".*") ||
+        result.innerText.includes("./")
+      ) {
+        errorAlert();
+      } else {
+        result.innerText = eval(result.innerText).toFixed(2);
+      }
+
+      break;
+    case "CE":
+    case "Backspace":
+      result.innerText = result.innerText.substring(0, result.innerText.length - 1);
+      break;
+    default:
+      result.innerText += valueInput;
+      break;
+  }
+}
+
+function errorAlert() {
+  setTimeout(() => {
+    const alert = document.getElementById("alert");
+    alert.style.opacity = "1";
+    if (alert.style.opacity == "1") {
+      setTimeout(() => {
+        alert.style.opacity = "0";
+      }, 1500);
+    }
+  });
+  clearTimeout();
+  clearDisplay();
+}
+
+function clearDisplay() {
+  result.innerText = "";
+}
